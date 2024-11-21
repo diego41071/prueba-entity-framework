7546b49c8dd4363705b6ae07ec279703f82b777e
diego41071 <alexander.diego.2007@hotmail.com>
2024-11-21 16:43:26 -0500

    primer commit

---
+++ EmpresaApp/Models/Empresa.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EmpresaApp.Models
+{
+    public class Empresa
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int EmpresaID { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Nombre { get; set; }
+
+        [Required]
+        public int Codigo { get; set; }
+
+        [StringLength(200)]
+        public string Direccion { get; set; }
+
+        [StringLength(15)]
+        public string Telefono { get; set; }
+
+        [StringLength(50)]
+        public string Ciudad { get; set; }
+
+        [StringLength(50)]
+        public string Departamento { get; set; }
+
+        [StringLength(50)]
+        public string Pais { get; set; }
+
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime FechaCreacion { get; set; }
+
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime FechaModificacion { get; set; }
+    }
+}
